da963a86af8a4bd27e30c551ebff3bd4e681da9b
suixing <suixing@uni-ubi.com>
2023-04-23 16:49:39 +0800

    feat: 更新测试专用脚本

---
+++ private_deploy_for_test.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+branch_name=private/pcTest/master
+merge_target_branch=private/master
+
+# 要修改的ip
+target_ip=10.10.0.119
+# 修改后的ip
+modified_ip=192.168.42.119
+function title {
+  echo 
+  echo "###############################################################################"
+  echo "## $1"
+  echo "###############################################################################" 
+  echo 
+}
+
+function operateGit {
+  yes | git checkout $merge_target_branch
+  yes | git push -d origin $branch_name
+  yes | git branch -D $branch_name
+  yes | git checkout -b $branch_name
+  yes | git push --set-upstream origin $branch_name
+}
+
+function submitCode {
+  title "提交代码"
+  yes | git add .
+  yes | git commit -m 'feat: 修改登录地址'  --no-verify
+  yes | git push
+}
+
+title "进入开发者平台，创建私有分支>>>>>>>>"
+cd ~/code/uniubi-wo-controller-web
+operateGit
+
+title "修改私有文件中的登录地址"
+gsed -i "s/$target_ip:8088/$modified_ip:8088/g" ~/code/uniubi-wo-controller-web/.umirc.private.ts
+
+title "提交代码"
+yes | git add .
+yes | git commit -m 'feat: 修改登录地址'  --no-verify
+yes | git push
+
+title "打包代码"
+yarn build:private
+yes | tar -czvf controller.tar.gz dist/
+yes | mv controller.tar.gz ~/Desktop/distAll
+
+title "进入物联网，创建私有分支>>>>>>>>"
+cd ~/code/uniubi-wo-isv-web
+operateGit
+
+title "修改私有文件中的登录地址"
+gsed -i "s/$target_ip:8088/$modified_ip:8088/g" ~/code/uniubi-wo-isv-web/.umirc.private.ts
+
+title "提交代码"
+yes | git add .
+yes | git commit -m 'feat: 修改登录地址'  --no-verify
+yes | git push
+
+title "打包代码"
+yarn build:private
+yes | tar -czvf isv.tar.gz dist/
+yes | mv isv.tar.gz ~/Desktop/distAll
+
+title "进入统一登录，创建私有分支>>>>>>>>"
+cd ~/code/wo-developer-signin-web 
+operateGit
+
+title "修改私有文件中的登录地址"
+gsed -i "s/$target_ip:8086/$modified_ip:8086/g" ~/code/wo-developer-signin-web/config/private.env.js
+
+title "提交代码"
+yes | git add .
+yes | git commit -m 'feat: 修改登录地址'  --no-verify
+yes | git push
+
+title "打包代码"
+yarn build:private
+yes | tar -czvf signin.tar.gz dist/
+yes | mv signin.tar.gz ~/Desktop/distAll
+
